e96a3cd1621cb65717e3c6eda661e0b3a3896e82
mauritshdb <mauritshudaibi@gmail.com>
2022-11-23 08:58:26 +0700

    getting started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { Axios } from 'axios';
 
 function App() {
+
+  const getData = () => {
+    Axios({
+      method: 'get',
+      url: 'http://localhost:7777/product',
+    })
+      .then(function (response) {
+        setData(response.data.data)
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Test</h1>
+    </>
   );
 }
 
